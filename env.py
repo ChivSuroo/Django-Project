b367282f8bb94741366fad748030c05d8a7773f7
Suroo <SDHUGHES1998@GMAIL.COM>
2022-05-25 11:53:21 +0000

    added apps, linked database

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://fzappuqypokaha:a56eeff1838dd80bd3faf94e82d5157c2c0a0cef3c359e91b397f8c3a6d8fe27@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d6eg0j8udl7c1h"
+os.environ["SECRET_KEY"] = "secretkey1"
